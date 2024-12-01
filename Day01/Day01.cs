a090d696e15f3d4f48b37e7a7427cc8b7f929890
Bruno <bruno.uy@gmail.com>
2024-12-01 10:32:57 -0300

    Day01 part2

--- Day01/Day01.cs
+++ Day01/Day01.cs
@@ -14,4 +14,17 @@ public sealed class Day01
 
         Assert.AreEqual(2066446, actual.Single());
     }
+    [TestMethod]
+    public void TestMethod2()
+    {
+        var actual =
+            from line in File.ReadLines("input.txt")
+            let s = line.Split(' ', StringSplitOptions.RemoveEmptyEntries)
+            group s by 0 into g
+            let left = g.Select(s => int.Parse(s[0]))
+            let right = g.Select(s => int.Parse(s[1]))
+            select left.GroupJoin(right, x => x, x => x, (l, rs) => l * rs.Count()).Sum();
+
+        Assert.AreEqual(24931009, actual.Single());
+    }
 }
