3d7e68c2246ca4c6416721c309a283ce877484d9
Bruno <bruno.uy@gmail.com>
2024-12-01 10:20:54 -0300

    Day01 part1

---
+++ Day01/Day01.cs
@@ -0,0 +1,17 @@
+﻿[TestClass]
+public sealed class Day01
+{
+    [TestMethod]
+    public void TestMethod1()
+    {
+        var actual =
+            from line in File.ReadLines("input.txt")
+            let s = line.Split(' ', StringSplitOptions.RemoveEmptyEntries)
+            group s by 0 into g
+            let left = g.Select(s => int.Parse(s[0]))
+            let right = g.Select(s => int.Parse(s[1]))
+            select left.Order().Zip(right.Order()).Select(p => Math.Abs(p.First - p.Second)).Sum();
+
+        Assert.AreEqual(2066446, actual.Single());
+    }
+}
