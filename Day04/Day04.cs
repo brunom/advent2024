cea34ba20740a1062ff50425c938d85f402055cf
Bruno <bruno.uy@gmail.com>
2024-12-05 19:37:33 -0300

    Day04 part2

--- Day04/Day04.cs
+++ Day04/Day04.cs
@@ -3,7 +3,7 @@ using System.Text.RegularExpressions;
 
 namespace Day04
 {
-    public unsafe class UnitTest1
+    public unsafe class Day04
     {
         [Fact]
         public void Test1()
@@ -32,6 +32,26 @@ namespace Day04
                     select (char)pointer[e.r * cols + e.c]);
 
             Assert.Equal(2603, q.Select(s => Regex.Matches(s, "XMAS").Count() + Regex.Matches(s, "SAMX").Count()).Sum());
+
+            int count = 0;
+            for (int r = 1; r < rows - 1; r++)
+            {
+                for (int c = 1; c < cols - 1; ++c)
+                {
+                    if (file[r * cols + c] != (byte)'A')
+                        continue;
+                    byte NE = file[(r - 1) * cols + c + 1];
+                    byte SE = file[(r + 1) * cols + c + 1];
+                    byte SW = file[(r + 1) * cols + c - 1];
+                    byte NW = file[(r - 1) * cols + c - 1];
+
+                    if (
+                        (NE == (byte)'M' && SW == (byte)'S' || NE == (byte)'S' && SW == (byte)'M') &&
+                        (SE == (byte)'M' && NW == (byte)'S' || SE == (byte)'S' && NW == (byte)'M'))
+                        count += 1;
+                }
+            }
+            Assert.Equal(1965, count);
         }
     }
 }
