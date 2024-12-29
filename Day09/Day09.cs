3db9d1b8eb1d1735c21715db9c795b61e4adde95
Bruno <bruno.uy@gmail.com>
2024-12-29 16:32:37 -0300

    Day09 wip

---
+++ Day09/Day09.cs
@@ -0,0 +1,23 @@
+﻿using System.Diagnostics;
+
+namespace Day09
+{
+    public class Day09
+    {
+        [Fact]
+        public void Test1()
+        {
+            string file = File.ReadAllText("input.txt");
+            Trace.Assert(file.Length % 2 == 1); // starts and ends with a file
+            int IDs = file.Length / 2;
+            int iBlock = 0;
+            Int64 checksum = 0;
+            for (int ID = 0; ID < IDs; ++ID)
+            {
+                checksum += ID * 
+            }
+            Assert.Equal(-1, checksum);
+
+        }
+    }
+}
