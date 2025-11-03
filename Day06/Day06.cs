6954c857c879f56d84d9d5e0565528b9d620bf0d
brunom <bruno.uy@gmail.com>
2025-11-02 21:59:27 -0300

    Day06 slow

--- Day06/Day06.cs
+++ Day06/Day06.cs
@@ -7,18 +7,23 @@ namespace Day06
 {
     public class Day06
     {
-        (int row, int col) Forward(int row, int col, char dir)
+        readonly string dirs = "^>v<";
+        static (int row, int col) Proj((int row, int col, char dir) x)
         {
-            return dir switch
+            return (x.row, x.col);
+        }
+        static (int row, int col, char dir) Forward((int row, int col, char dir) x)
+        {
+            return x.dir switch
             {
-                '^' => (row - 1, col + 0),
-                'v' => (row + 1, col + 0),
-                '<' => (row + 0, col - 1),
-                '>' => (row + 0, col + 1),
+                '^' => (x.row - 1, x.col + 0, x.dir),
+                'v' => (x.row + 1, x.col + 0, x.dir),
+                '<' => (x.row + 0, x.col - 1, x.dir),
+                '>' => (x.row + 0, x.col + 1, x.dir),
                 _ => throw new InvalidDataException(),
             };
         }
-        char Turn(char dir)
+        static char Turn(char dir)
         {
             return dir switch
             {
@@ -38,39 +43,73 @@ namespace Day06
                 .ToArray();
             int rows = map.Length;
             int cols = map[0].Length;
-            bool Outside(int row, int col) => row < 0 || row >= rows || col < 0 || col >= cols;
-            (int row, int col) pos = (0, 0);
+            bool Outside((int row, int col) x) => x.row < 0 || x.row >= rows || x.col < 0 || x.col >= cols;
+            char ReadMap((int row, int col) x) => map[x.row][x.col];
+            (int row, int col, char dir) curr = default;
             for (int row = 0; row < rows; ++row)
             {
                 for (int col = 0; col < cols; ++col)
                 {
-                    switch (map[row][col])
-                    {
-                        case '^':
-                            pos = (row, col);
-                            break;
-                    }
+                    char ch = map[row][col];
+                    if (dirs.Contains(ch))
+                        curr = (row, col, ch);
                 }
             }
-            char dir = '^'; // starts up
             HashSet<(int row, int col, char dir)> route = [];
+            bool RouteContainsAny((int row, int col) x) => dirs.Any(dir => route.Contains((x.row, x.col, dir)));
+            (int row, int col, char dir)? obstruction = null;
+            HashSet<(int row, int col, char dir)> route_after_obstruction = [];
+            int nobstructions = 0;
             while (true)
             {
-                if (!route.Add((pos.row, pos.col, dir)))
-                    break;
-                var forward = Forward(pos.row, pos.col, dir);
-                if (Outside(forward.row, forward.col))
-                    break;
-                if (map[forward.row][forward.col] == '#')
+                if (obstruction == null)
                 {
-                    dir = Turn(dir);
+                    // Shouldn't happen because only obstructions generate loops.
+                    if (!route.Add(curr))
+                        break;
                 }
                 else
                 {
-                    pos = forward;
+                    if (route.Contains(curr) || !route_after_obstruction.Add(curr))
+                    {
+                        ++nobstructions;
+                        curr = obstruction.Value;
+                        obstruction = null;
+                        route_after_obstruction.Clear();
+                        continue;
+                    }
                 }
+                var forward = Forward(curr);
+                if (Outside(Proj(forward)))
+                {
+                    if (obstruction == null)
+                        break;
+                    curr = obstruction.Value;
+                    obstruction = null;
+                    route_after_obstruction.Clear();
+                    continue;
+                }
+                char ch = ReadMap(Proj(forward));
+                if (ch == '#' ||
+                    obstruction != null && Proj(obstruction.Value) == Proj(forward))
+                {
+                    curr.dir = Turn(curr.dir);
+                    continue;
+                }
+                // Don't put obstruction where we already tried
+                if (RouteContainsAny(Proj(forward)) || obstruction != null)
+                {
+                    curr = forward;
+                    continue;
+                }
+                obstruction = forward;
+                curr.dir = Turn(curr.dir);
             }
-            Assert.Equal(4454, route.Select(x => (x.row, x.col)).Distinct().Count());
+            //Assert.Equal(41, route.Select(Proj).Distinct().Count());
+            //Assert.Equal(6, nobstructions);
+
+            Assert.Equal(4454, route.Select(Proj).Distinct().Count());
+            Assert.Equal(1503, nobstructions);
         }
     }
 }
